c27c3a845fb315c9e4d546e0155e5d169b6070f2
SamnangKong426 <samnangkong499@gmail.com>
2025-05-16 22:39:37 +0700

    remove unuse

--- lib/models/race_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sport_chrono/models/participant_model.dart';
-
-class Race {
-  final String sportType;
-  final List<Participant> participants;
-
-  Race({required this.participants, required this.sportType});
-}
